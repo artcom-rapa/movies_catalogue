a3f3ff126613f6bd1cb4da639ce571de919a68d5
Ada Rapa <ada.rapa@interia.pl>
2020-06-29 04:14:34 +0200

    Add file movie_details.html and movie list selection in files: homapage.html, main.py, tmdb_client.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import tmdb_client
+import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(how_many=8)
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get("list_type", "popular")
+    lists = ["popular", "top_rated", "upcoming", "now_playing"]
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    random.shuffle(movies)
+    return render_template("homepage.html", movies=movies, current_list=selected_list, lists=lists)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    movies = tmdb_client.get_movies(how_many=8, list_type="popular")
+    return render_template("homepage.html", movies=movies, current_list="popular")
 
 
 @app.context_processor
@@ -22,6 +32,15 @@ def utility_processor():
     def tmdb_movie_info(path, size):
         return tmdb_client.get_poster_url(path, size)
     return {"tmdb_movie_info": tmdb_movie_info}
+
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id, how_many=4)
+    backdrops = tmdb_client.get_single_movie_images(movie_id)
+    random.shuffle(backdrops['backdrops'])
+    return render_template("movie_details.html", movie=details, cast=cast, backdrops=backdrops)
 
 
 if __name__ == '__main__':
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,11 +1,12 @@
 import requests
 
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YThlYmJiMGFhYTQxZTNhNWM3MzhlZWU2MTRlOGVjMCIsInN1YiI6IjVlZTUyMGQ5MGNiMzM1MDAyMmJkM2UwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8Jwro20e9Dcr410aJ_LVBQQRkG7uyZtwuYSUm2mqUfI"
 
+"""
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YThlYmJiMGFhYTQxZTNhNWM3MzhlZWU2MTRlOGVjMCIsInN1YiI6IjVlZTUyMGQ5MGNiMzM1MDAyMmJkM2UwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8Jwro20e9Dcr410aJ_LVBQQRkG7uyZtwuYSUm2mqUfI"
     headers = {
-        "Authorization": f"Bearer {api_token}"
+        "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()
@@ -13,6 +14,22 @@ def get_popular_movies():
 
 def get_movies(how_many):
     data = get_popular_movies()
+    return data["results"][:how_many]
+"""
+
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_movies(how_many, list_type="popular"):
+    data = get_movies_list(list_type)
     return data["results"][:how_many]
 
 
@@ -25,3 +42,29 @@ def get_movie_info(title, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{title}"
 
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_single_movie_cast(movie_id, how_many):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"][:how_many]
+
+
+def get_single_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
